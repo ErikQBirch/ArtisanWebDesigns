6cb0946095934c5d3d746ab7fe2eaa6c6b68efaa
erikqbirch <erikqbirch@gmail.com>
2023-08-29 15:50:33 -0600

    Small Fix

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -11,7 +11,7 @@ const pageStuff = {
   hero: function(
     hero_tag = helperFunctions.generateElement('div',"hero"),
     figure_tag = helperFunctions.generateElement('figure'),
-    img_tag = helperFunctions.generateElement('img',"","","","../assets/resources/imgs/heroPics/Hero5.webp"),
+    img_tag = helperFunctions.generateElement('img',"","","","../assets/resources/imgs/heroPics/hero5.webp"),
     banner_tag = helperFunctions.generateElement('div', "banner","","I can make you a website worthy of your content!")
   ){
     figure_tag.appendChild(img_tag);
